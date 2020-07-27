e1b60d984b1b44106339bbd13fde5ad2d9b1a902
Gastón Milano <gmilano@genexus.com>
2020-07-27 17:34:32 -0300

    no message

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+
+export var   SettingKeys = {
+    S3_BUCKET : "gxBucket",
+    S3_SECRET_KEY : "gxS3SecretKey",
+    S3_ACCESS_KEY : "gxS3AccessKey",
+    ENABLE_S3: "gxS3Enabled" ,
+    DESIGN_QUEUE: "DesignOpsQueue",
+};
+
