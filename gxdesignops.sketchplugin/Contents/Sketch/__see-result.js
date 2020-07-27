c09dfed1ddc158cbb06ab09dc41325a56a422fae
Gastón Milano <gmilano@genexus.com>
2020-07-27 20:09:04 -0300

    Fixes

--- gxdesignops.sketchplugin/Contents/Sketch/__see-result.js
+++ gxdesignops.sketchplugin/Contents/Sketch/__see-result.js
@@ -1457,65 +1457,25 @@ var UI = __webpack_require__(/*! sketch/ui */ "sketch/ui");
 var zip = __webpack_require__(/*! jszip */ "./node_modules/jszip/dist/jszip.min.js");
 
 /* harmony default export */ __webpack_exports__["default"] = (function () {
-  sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("Post the result ! 💚");
-  /*
-    var dateValue = execSync("date -R").toString().replace(/\r?\n|\r/, "");
-    console.log("Date:" + dateValue);
-    var bucket = "gx-designops"
-    var fileName = "SketchSampleBrasil.sketch";
-    var file = `/Users/gmilano/Documents/DesignSketchs/${fileName}`;
-    var s3Secret = "yU11uEqs3LLKVeT7t6YAZvzr+Y7W4UiUjH2Re+5B";
-    var resource = `/${bucket}/${fileName}`;
-    var contentType = "application/x-compressed-tar";
-    var s3Secret = "yU11uEqs3LLKVeT7t6YAZvzr+Y7W4UiUjH2Re+5B";
-    var signature;
-    var stringToSign = `"PUT\n\napplication/x-compressed-tar\n${dateValue}\n${resource}"`;
-    console.log(stringToSign);
-    var signMethod = `echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`;
-    console.log(signMethod);
-    var signatureObj = execSync(signMethod);
-    if (signatureObj)
-    {
-      signature = signatureObj.toString().replace(/\r?\n|\r/, "");
-      console.log("Signature: " + signature.toString());
-  
-    }  
-    var s3Key = "AKIAQQE2R2N2I3ZJ2OLW";
-    const curl_command =  `curl -X PUT -T "${file}" -H "Host: ${bucket}.s3.amazonaws.com" -H "Date: ${dateValue}" -H "Content-Type: ${contentType}" -H "Authorization: AWS ${s3Key}:${signature}" https://${bucket}.s3.amazonaws.com/${fileName}`;
-    console.log(curl_command);
-    var test = execSync(curl_command);
-    if (test)
-      console.log(test.toString()); 
-      */
-  //const curl_command = 'curl -X PUT -T ${file} -H "Host: ${bucket}.s3.amazonaws.com"  -H "Date: ${dateValue}" -H "Content-Type: ${contentType}" -H "Authorization: AWS ${s3Key}:${signature}" https://${bucket}.s3.amazonaws.com/${bucketfilename}';
-  //var p = execSync("echo hola");
-  //const curl_command_2 = "echo hola"
-  //var p = execSync(curl_command_2, {
-  // shell: true,
-  // stdio: true,
-  //});
-  //if (p)
-  // console.log(p);
+  var doc = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.getSelectedDocument();
+  var queuePath = Object(_utils__WEBPACK_IMPORTED_MODULE_2__["getQueuePath"])(queuePath);
+  var fileName;
 
-  sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("Opening the result ! 💚");
-});
-/* const doc = sketch.getSelectedDocument()
- var queuePath = getQueuePath(queuePath);
- var fileName;
- ({ fileName, queuePath } = getFileAndQueueName(doc, queuePath));
-  const urlPath = queuePath + fileName.replace(".sketch", ".url");
- console.log("open !! " + urlPath);
-  const spawn = spawnSync('open', [
-   "'" + urlPath + "'"
- ], { shell: true });
+  var _getFileAndQueueName = Object(_utils__WEBPACK_IMPORTED_MODULE_2__["getFileAndQueueName"])(doc, queuePath);
+
+  fileName = _getFileAndQueueName.fileName;
+  queuePath = _getFileAndQueueName.queuePath;
+  var urlPath = queuePath + fileName.replace(".sketch", ".url");
+  console.log("open !! " + urlPath);
+  var spawn = Object(_skpm_child_process__WEBPACK_IMPORTED_MODULE_1__["spawnSync"])('open', ["'" + urlPath + "'"], {
+    shell: true
+  });
+
   if (spawn.status > 0) {
-   console.log(Error(spawn.stderr));
-   sketch.UI.message("😔 Some error occurs, see console for further details");
- }
- else
-   sketch.UI.message("Opening the result ! 💚");
-}
-*/
+    console.log(Error(spawn.stderr));
+    sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("😔 Some error occurs, see console for further details");
+  } else sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("Opening the result ! 💚");
+});
 
 /***/ }),
 
--- src/see-result.js
+++ src/see-result.js
@@ -13,53 +13,7 @@ var zip = require("jszip");
 
 export default function () {
   
-  sketch.UI.message("Post the result ! 💚")
-
-/*
-  var dateValue = execSync("date -R").toString().replace(/\r?\n|\r/, "");
-  console.log("Date:" + dateValue);
-  var bucket = "gx-designops"
-  var fileName = "SketchSampleBrasil.sketch";
-  var file = `/Users/gmilano/Documents/DesignSketchs/${fileName}`;
-  var s3Secret = "yU11uEqs3LLKVeT7t6YAZvzr+Y7W4UiUjH2Re+5B";
-  var resource = `/${bucket}/${fileName}`;
-  var contentType = "application/x-compressed-tar";
-  var s3Secret = "yU11uEqs3LLKVeT7t6YAZvzr+Y7W4UiUjH2Re+5B";
-  var signature;
-  var stringToSign = `"PUT\n\napplication/x-compressed-tar\n${dateValue}\n${resource}"`;
-  console.log(stringToSign);
-  var signMethod = `echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`;
-  console.log(signMethod);
-  var signatureObj = execSync(signMethod);
-  if (signatureObj)
-  {
-    signature = signatureObj.toString().replace(/\r?\n|\r/, "");
-    console.log("Signature: " + signature.toString());
-
-  }  
-  var s3Key = "AKIAQQE2R2N2I3ZJ2OLW";
-  const curl_command =  `curl -X PUT -T "${file}" -H "Host: ${bucket}.s3.amazonaws.com" -H "Date: ${dateValue}" -H "Content-Type: ${contentType}" -H "Authorization: AWS ${s3Key}:${signature}" https://${bucket}.s3.amazonaws.com/${fileName}`;
-  console.log(curl_command);
-  var test = execSync(curl_command);
-  if (test)
-    console.log(test.toString()); 
-    */
-  
-  //const curl_command = 'curl -X PUT -T ${file} -H "Host: ${bucket}.s3.amazonaws.com"  -H "Date: ${dateValue}" -H "Content-Type: ${contentType}" -H "Authorization: AWS ${s3Key}:${signature}" https://${bucket}.s3.amazonaws.com/${bucketfilename}';
-  //var p = execSync("echo hola");
-  //const curl_command_2 = "echo hola"
-  //var p = execSync(curl_command_2, {
-   // shell: true,
-   // stdio: true,
-   
-  //});
-  //if (p)
-   // console.log(p);
-  
-  sketch.UI.message("Opening the result ! 💚");
-}
-
- /* const doc = sketch.getSelectedDocument()
+  const doc = sketch.getSelectedDocument()
   var queuePath = getQueuePath(queuePath);
   var fileName;
   ({ fileName, queuePath } = getFileAndQueueName(doc, queuePath));
@@ -78,6 +32,6 @@ export default function () {
   else
     sketch.UI.message("Opening the result ! 💚");
 }
-*/
+
 
 
